f78c6917f3a6aebd441b4a764e16130e470a191f
jonathan-robin <jonathan@levelup-analytics.com>
2025-02-17 16:59:43 +0400

    add validator to patient entity

--- patient-service/src/main/java/com/service/model/Patient.java
+++ patient-service/src/main/java/com/service/model/Patient.java
@@ -3,6 +3,11 @@ package com.service.model;
 import java.time.LocalDate;
 
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -27,21 +32,29 @@ public class Patient {
     @Column(name = "id")
     private Long id;
 
+    @NotBlank(message= "Le nom est obligatoire")
     @Column(name = "last_name")
     private String lastName;
 
     @Column(name = "first_name")
+    @NotBlank(message="Le prénom est obligatoire")
     private String firstName;
 
     @Column(name = "birth_date")
+    @NotNull(message = "La date de naissance est obligatoire")
+    @Past(message = "La date de naissance doit être dans le passé")
     private LocalDate dateOfBirth;
 
     @Column(name = "gender")
+    @NotBlank(message = "Le genre est obligatoire")
+    @Pattern(regexp = "M|F", message = "Le genre doit être 'M' ou 'F'")
     private String genre;
 
     @Column(name = "address")
+    @Size(max = 255, message = "L'adresse ne peut pas dépasser 255 caractères")
     private String address;
 
     @Column(name = "phone_number")
+    @Pattern(regexp = "^\\+?\\d{10,15}$", message = "Le numéro de téléphone est invalide")
     private String phoneNumber;
 }
